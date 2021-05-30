305353004a899d4c42035a77c7decf0e3656156b
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2021-05-30 21:47:00 +0100

    modified server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import numpy as np
 import pandas as pd
 from utils import load_estimator
@@ -25,33 +25,58 @@ def predict():
 
     return render_template('index.html', prediction_text=f'{(final_pred)}')
 
-@app.route('/predict_API')
+@app.route('/predict_API', methods=['POST'])
 def predict_API() -> 'json_object':
     """
     ================================================================
     Get the predictions for new data. The data is a pandas DataFrame.
     It returns json object
     """
-    # clean the data
-    test_data = clean_test_data('./data/test.csv')
-    # load estimator
+    # load the estimator
     estimator = load_estimator()
-    clf = estimator.get('clf')
-    # convert dataframe to numpy array
-    X = test_data.to_numpy()
-    # make predictions
-    pred = clf.predict(X)
-    test_data['Predictions'] = pred
-    test_data = test_data.reset_index()       
-    path= './data/pred_data.json'   # path
-    # save data as json
-    json_data = save_df_as_json(test_data, path)
-    # load the json file
-    json_data = load_json_data(path)
-    return jsonify(json_data)
+    clf = estimator['clf']
+    le_gender = estimator['le_gender']
+    le_married = estimator['le_married']
+    le_dep = estimator['le_dep']
+    le_edu = estimator['le_edu']
+    le_self_emp = estimator['le_self_emp']
+    le_pr_ar = estimator['le_pr_ar']
 
-@app.route('/predict_API_2')
-def predict_API_2() -> 'json_object':
+    # get data from postman
+    req = request.get_json()
+    test_data = {}
+    # add values to the dictionary
+    test_data['Gender'] = [req['Gender']]
+    test_data['Married'] = [req['Married']]
+    test_data['Dependents'] = [req['Dependents']]
+    test_data['Education'] = [req['Education']]
+    test_data['Self_Employed'] = [req['Self_Employed']]
+    test_data['ApplicantIncome'] = [req['ApplicantIncome']]
+    test_data['CoapplicantIncome'] = [req['CoapplicantIncome']]
+    test_data['LoanAmount'] = [req['LoanAmount']]
+    test_data['Loan_Amount_Term'] = [req['Loan_Amount_Term']]
+    test_data['Credit_History'] = [req['Credit_History']]
+    test_data['Property_Area'] = [req['Property_Area']]
+    # convert to dataframe
+    df = pd.DataFrame.from_dict(test_data, orient='columns')
+    new_data = df.to_numpy()
+    # encode the categorical features
+    new_data[:, 0] = le_gender.transform(new_data[:, 0])
+    new_data[:, 1] = le_married.transform(new_data[:, 1])
+    new_data[:, 2] = le_dep.transform(new_data[:, 2])
+    new_data[:, 3] = le_edu.transform(new_data[:, 3])
+    new_data[:, 4] = le_self_emp.transform(new_data[:, 4])
+    new_data[:, 10] = le_pr_ar.transform(new_data[:, 10])
+
+    # make predictions
+    pred = clf.predict(new_data)
+    test_data['Prediction'] = pred
+    test_data = test_data['Prediction']
+    result = ['Yes' if test_data == 1 else 'No']
+    return jsonify({'Prediction': result})
+
+@app.route('/predict_test_data')
+def predict_test_data() -> 'json_object':
     """
     ==================================================================
     Get the predictions for new data. The data is a pandas DataFrame.
